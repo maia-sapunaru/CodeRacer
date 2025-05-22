c5799f1d593007f1cdab398a8d2b2c1596719c7a
Maia Sapunaru <sapunarumaia@gmail.com>
2025-05-23 00:19:45 +0300

    first commit

---
+++ include/Aggressive.h
@@ -0,0 +1,17 @@
+//
+// Created by Maia Sapunaru on 22.05.2025.
+//
+
+#ifndef AGGRESSIVE_H
+#define AGGRESSIVE_H
+#include "DrivingTechnique.h"
+
+
+class Aggressive : public DrivingTechnique {
+public:
+    void drive(Car& car) const override;
+};
+
+
+
+#endif //AGGRESSIVE_H
